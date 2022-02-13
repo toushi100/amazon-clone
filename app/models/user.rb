cd8ecf058bc634b3e3c62b277ceb3e56a9bb8df3
toushi100 <toushi2009@gmail.com>
2022-02-13 19:08:04 +0200

    calling job from users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    after_save do |job|
+        p"===================================================================="
+        p"this is the after save"
+        SendingMailsJob.perform_later(self)
+    end 
 end
